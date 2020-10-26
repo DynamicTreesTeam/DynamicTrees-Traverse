72e94f869a300ffda54d505987f3c1dd5f361c70
Harleyoc1 <harley.oconnor@icloud.com>
2020-10-26 11:10:01 +0000

    Bump version to 2.1 and change update checker location.

--- src/main/java/dttraverse/DynamicTreesTraverse.java
+++ src/main/java/dttraverse/DynamicTreesTraverse.java
@@ -10,12 +10,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid=DynamicTreesTraverse.MOD_ID, name = DynamicTreesTraverse.NAME, version = DynamicTreesTraverse.VERSION, dependencies = DynamicTreesTraverse.DEPENDENCIES, updateJSON = "http://harleyoconnor.com/mods/minecraft/dynamic-trees-traverse/version-info.json")
+@Mod(modid=DynamicTreesTraverse.MOD_ID, name = DynamicTreesTraverse.NAME, version = DynamicTreesTraverse.VERSION, dependencies = DynamicTreesTraverse.DEPENDENCIES, updateJSON = "https://github.com/Harleyoc1/DynamicTreesVersionInfo/blob/master/Add-ons/Traverse.json?raw=true")
 public final class DynamicTreesTraverse {
 
     public static final String MOD_ID = "dttraverse";
     public static final String NAME = "Dynamic Trees Traverse";
-    public static final String VERSION = "1.12.2-2.0";
+    public static final String VERSION = "1.12.2-2.1";
     public static final String DEPENDENCIES = ModConstants.REQAFTER + ModConstants.DYNAMICTREES_LATEST + ModConstants.NEXT + ModConstants.REQAFTER + "traverse";
 
     @Mod.Instance
