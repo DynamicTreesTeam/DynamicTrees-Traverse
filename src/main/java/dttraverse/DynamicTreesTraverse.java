66ffa8d4a6198634cf1ec84c583e93193cab9bc2
Harleyoc1 <harley.oconnor@icloud.com>
2021-07-07 14:16:19 +0100

    Added block and item tags.

--- src/main/java/dttraverse/DynamicTreesTraverse.java
+++ src/main/java/dttraverse/DynamicTreesTraverse.java
@@ -1,7 +1,11 @@
 package dttraverse;
 
+import com.ferreusveritas.dynamictrees.DynamicTrees;
 import com.ferreusveritas.dynamictrees.api.registry.RegistryHandler;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(DynamicTreesTraverse.MOD_ID)
 public final class DynamicTreesTraverse {
@@ -10,6 +14,11 @@ public final class DynamicTreesTraverse {
 
     public DynamicTreesTraverse() {
         RegistryHandler.setup(MOD_ID);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::gatherData);
+    }
+
+    private void gatherData(final GatherDataEvent event) {
+        DynamicTrees.gatherTagGenerators(MOD_ID, event);
     }
 
 }
