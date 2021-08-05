748f22e32588fee3a6726cf2f82dc7bb521e03e4
Harleyoc1 <harley.oconnor@icloud.com>
2021-08-05 16:04:30 +0100

    Switched to Kotlin build script.

---
+++ build.gradle.kts
@@ -0,0 +1,273 @@
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import com.matthewprenger.cursegradle.*
+import java.io.InputStreamReader
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+
+fun property(key: String) = project.findProperty(key).toString()
+
+plugins {
+    id("java")
+    id("net.minecraftforge.gradle")
+    id("idea")
+    id("maven-publish")
+    id("com.matthewprenger.cursegradle") version "1.4.0"
+}
+
+repositories {
+    maven("https://ldtteam.jfrog.io/ldtteam/modding/")
+    maven("https://maven.tehnut.info")
+    maven("https://www.cursemaven.com")
+    maven("https://harleyoconnor.com/maven")
+}
+
+val modName = property("modName")
+val modId = property("modId")
+val modVersion = property("modVersion")
+val mcVersion = property("mcVersion")
+
+version = "$mcVersion-$modVersion"
+group = property("group")
+
+minecraft {
+    mappings("official", mcVersion)
+    accessTransformer(file("src/main/resources/META-INF/accesstransformer.cfg"))
+
+    runs {
+        create("client") {
+            workingDirectory = file("run").absolutePath
+
+            property("forge.logging.markers", "SCAN,REGISTRIES,REGISTRYDUMP")
+            property("forge.logging.console.level", "debug")
+
+            property("mixin.env.remapRefMap", "true")
+            property("mixin.env.refMapRemappingFile", "${buildDir}/createSrgToMcp/output.srg")
+
+            if (project.hasProperty("mcUuid")) {
+                args("--uuid", property("mcUuid"))
+            }
+            if (project.hasProperty("mcUsername")) {
+                args("--username", property("mcUsername"))
+            }
+            if (project.hasProperty("mcAccessToken")) {
+                args("--accessToken", property("mcAccessToken"))
+            }
+
+            mods {
+                create(modId) {
+                    source(sourceSets.main.get())
+                }
+            }
+        }
+
+        create("server") {
+            workingDirectory = file("run").absolutePath
+
+            property("forge.logging.markers", "SCAN,REGISTRIES,REGISTRYDUMP")
+            property("forge.logging.console.level", "debug")
+
+            property("mixin.env.remapRefMap", "true")
+            property("mixin.env.refMapRemappingFile", "${buildDir}/createSrgToMcp/output.srg")
+
+            mods {
+                create(modId) {
+                    source(sourceSets.main.get())
+                }
+            }
+        }
+
+        create("data") {
+            workingDirectory = file("run").absolutePath
+
+            property("forge.logging.markers", "SCAN,REGISTRIES,REGISTRYDUMP")
+            property("forge.logging.console.level", "debug")
+
+            property("mixin.env.remapRefMap", "true")
+            property("mixin.env.refMapRemappingFile", "${buildDir}/createSrgToMcp/output.srg")
+
+            args("--mod", modId, "--all", "--output", file("src/generated/resources/"))
+
+            mods {
+                create(modId) {
+                    source(sourceSets.main.get())
+                }
+            }
+        }
+    }
+}
+
+sourceSets.main.get().resources {
+    srcDir("src/generated/resources")
+}
+
+dependencies {
+    // Not sure if we need this one, what is a "forge" anyway?
+    minecraft("net.minecraftforge:forge:${mcVersion}-${property("forgeVersion")}")
+
+    // Compile Hwyla API, but don"t include in runtime.
+    compileOnly(fg.deobf("mcp.mobius.waila:Hwyla:${property("hwylaVersion")}:api"))
+    // At runtime, use the full Hwyla mod.
+    runtimeOnly(fg.deobf("mcp.mobius.waila:Hwyla:${property("hwylaVersion")}"))
+
+    // Compile JEI API, but don"t include in runtime.
+    compileOnly(fg.deobf("mezz.jei:jei-${mcVersion}:${property("jeiVersion")}:api"))
+    // At runtime, use the full JEI mod.
+    runtimeOnly(fg.deobf("mezz.jei:jei-${mcVersion}:${property("jeiVersion")}"))
+
+    // Compile full Dynamic Trees mod.
+    implementation(fg.deobf("com.ferreusveritas.dynamictrees:DynamicTrees-${mcVersion}:${property("dynamicTreesVersion")}"))
+    // At runtime, use full Dynamic Trees + mod for cacti.
+    runtimeOnly(fg.deobf("com.ferreusveritas.dynamictreesplus:DynamicTreesPlus-${mcVersion}:${property("dynamicTreesPlusVersion")}"))
+
+    // Compile full Traverse mod.
+    implementation(fg.deobf("curse.maven:Traverse-267769:3157857"))
+
+    // At runtime, use suggestion provider fix mod.
+    runtimeOnly(fg.deobf("com.harleyoconnor.suggestionproviderfix:SuggestionProviderFix:${mcVersion}-${property("suggestionProviderFixVersion")}"))
+}
+
+tasks.jar {
+    manifest.attributes(
+        "Specification-Title" to project.name,
+        "Specification-Vendor" to "Harley O'Connor",
+        "Specification-Version" to "1",
+        "Implementation-Title" to project.name,
+        "Implementation-Version" to project.version,
+        "Implementation-Vendor" to "Harley O'Connor",
+        "Implementation-Timestamp" to DateTimeFormatter.ISO_INSTANT.format(Instant.now())
+    )
+
+    archiveBaseName.set(modName)
+    finalizedBy("reobfJar")
+}
+
+java {
+    withSourcesJar()
+
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
+}
+
+fun readChangelog(): String? {
+    val versionInfoFile = file("version_info.json")
+    val jsonObject = Gson().fromJson(InputStreamReader(versionInfoFile.inputStream()), JsonObject::class.java)
+    return jsonObject
+        .get(mcVersion)?.asJsonObject
+        ?.get(project.version.toString())?.asString
+}
+
+tasks.withType(CurseUploadTask::class.java) {
+    onlyIf {
+        project.hasProperty("curseApiKey") && project.hasProperty("curseFileType")
+    }
+}
+
+curseforge {
+    if (!project.hasProperty("curseApiKey") || !project.hasProperty("curseFileType")) {
+        project.logger.log(LogLevel.WARN, "API Key or file type for CurseForge not detected; uploading will be disabled.")
+        return@curseforge
+    }
+
+    apiKey = property("curseApiKey")
+
+    project {
+        id = "407056"
+
+        addGameVersion("1.16.4")
+        addGameVersion(mcVersion)
+
+        changelog = readChangelog() ?: "No changelog provided."
+        changelogType = "markdown"
+        releaseType = property("curseFileType")
+
+        addArtifact(tasks.findByName("sourcesJar"))
+
+        mainArtifact(tasks.findByName("jar")) {
+            relations {
+                requiredDependency("dynamictrees")
+                requiredDependency("traverse-reforged")
+                optionalDependency("dynamictreesplus")
+            }
+        }
+    }
+}
+
+tasks.withType<GenerateModuleMetadata> {
+    enabled = false
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "$modName-$mcVersion"
+            version = modVersion
+
+            from(components["java"])
+
+            pom {
+                name.set(modName)
+                url.set("https://github.com/Harleyoc1/$modName")
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://mit-license.org")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Harleyoc1")
+                        name.set("Harley O'Connor")
+                        email.set("Harleyoc1@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/Harleyoc1/$modName.git")
+                    developerConnection.set("scm:git:ssh://github.com/Harleyoc1/$modName.git")
+                    url.set("https://github.com/Harleyoc1/$modName")
+                }
+            }
+
+            pom.withXml {
+                val element = asElement()
+
+                // Clear dependencies.
+                for (i in 0 until element.childNodes.length) {
+                    val node = element.childNodes.item(i)
+                    if (node?.nodeName == "dependencies") {
+                        element.removeChild(node)
+                    }
+                }
+            }
+        }
+    }
+    repositories {
+        maven("file:///${project.projectDir}/mcmodsrepo")
+        if (hasProperty("harleyOConnorMavenUsername") && hasProperty("harleyOConnorMavenPassword")) {
+            maven("https://harleyoconnor.com/maven") {
+                name = "HarleyOConnor"
+                credentials {
+                    username = property("harleyOConnorMavenUsername")
+                    password = property("harleyOConnorMavenPassword")
+                }
+            }
+        } else {
+            logger.log(LogLevel.WARN, "Credentials for maven not detected; it will be disabled.")
+        }
+    }
+}
+
+// Extensions to make CurseGradle extension slightly neater.
+
+fun CurseExtension.project(action: CurseProject.() -> Unit) {
+    this.project(closureOf(action))
+}
+
+fun CurseProject.mainArtifact(artifact: Task?, action: CurseArtifact.() -> Unit) {
+    this.mainArtifact(artifact, closureOf(action))
+}
+
+fun CurseArtifact.relations(action: CurseRelation.() -> Unit) {
+    this.relations(closureOf(action))
+}
